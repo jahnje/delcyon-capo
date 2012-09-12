5ed703fdb869110dae023ca11e9f0b28011befe9
Jeremiah Jahn <jeremiah@delcyon.com>
2012-09-12 07:00:00 -0500

    removed search element. As we use a declarative approach to resources, and don't need an active query like control element. Also renamed related files and tests.

--- tests/com/delcyon/capo/controller/elements/ResourceElementTest.java
+++ tests/com/delcyon/capo/controller/elements/ResourceElementTest.java
@@ -28,7 +28,7 @@ import com.delcyon.capo.controller.LocalRequestProcessor;
 import com.delcyon.capo.tests.util.TestServer;
 import com.delcyon.capo.tests.util.Util;
 
-public class SearchElementTest
+public class ResourceElementTest
 {
    
     @After
@@ -42,39 +42,11 @@ public class SearchElementTest
     @Test
     public void simpleTest() throws Exception
     {
-    	String test = "jdbc:hsqldb:file:testdb/testdb?user=user\\?&password&\\&\\!systems=\\=";
-    	String [] uriSplit = test.split("!(?<!\\\\!)");
-    	for (String uriSection : uriSplit)
-    	{
-    		System.out.println(uriSection);
-    	}
-    	String[] parameterSectionSplit = uriSplit[0].replaceAll("\\\\(?=!)", "").split("\\?(?<!\\\\\\?)");// now split off the parameter section of the first declaration of the URI
-    	for (String uriSection : parameterSectionSplit)
-    	{
-    		System.out.println("ps==>"+uriSection);
-    	}
-    	if (parameterSectionSplit.length > 1)
-    	{
-    		String[] parameterSplit = parameterSectionSplit[1].replaceAll("\\\\(?=\\?)", "").split("&(?<!\\\\&)");// now split off the parameters from parameter section
-    		for (String parameter : parameterSplit)
-    		{
-    			System.out.println("p==>"+parameter);
-    			String[] avp = parameter.replaceAll("\\\\(?=&)", "").split("=(?<!\\\\=)");// now split off the parameters from parameter section
-    			String parameterName = avp[0].replaceAll("\\\\(?==)", "");
-    			String parameterValue = "";
-    			if(avp.length > 1)
-    			{
-    				parameterValue = avp[1].replaceAll("\\\\(?==)", "");
-    			}
-    			System.out.println("\tname=> '"+parameterName+"'");
-    			System.out.println("\tvalue=> '"+parameterValue+"'");
-    		}
-    	}
     	
     	Util.copyTree("test-data/capo", "capo", true, true);
     	Util.copyTree("test-data/testdb", "testdb", true, true);
         TestServer.start();
-        Document document = CapoApplication.getDocumentBuilder().parse(new FileInputStream("test-data/test-search.xml"));
+        Document document = CapoApplication.getDocumentBuilder().parse(new FileInputStream("test-data/resource-element-test.xml"));
         LocalRequestProcessor localRequestProcessor = new LocalRequestProcessor();
         localRequestProcessor.process(document);
     }
