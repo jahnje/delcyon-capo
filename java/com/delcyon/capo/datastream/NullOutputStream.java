710ea2f32dc8ee8293323ae644519994365aecbe
Jeremiah Jahn <jeremiah@delcyon.com>
2012-11-25 10:46:17 -0600

    made more efficient, by implementing other methods.

--- java/com/delcyon/capo/datastream/NullOutputStream.java
+++ java/com/delcyon/capo/datastream/NullOutputStream.java
@@ -32,4 +32,16 @@ public class NullOutputStream extends OutputStream
 		
 	}
 
+	@Override
+	public void write(byte[] b) throws IOException
+	{
+	    
+	}
+	
+	@Override
+	public void write(byte[] b, int off, int len) throws IOException
+	{
+	    
+	}
+	
 }
