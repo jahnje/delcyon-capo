cc8fee048feb5f0a03a5343807f092c8cefefb17
jeremiah <jeremiah@10.0.1.3>
2014-09-28 13:17:00 -0500

    chnaged this to work as expected, as opposed to just figuring out how stuff works. Passes all tests now, but probably has some hidden bugs someplace. Esp in notifing parent of child additions.

--- java/com/delcyon/capo/resourcemanager/types/JcrResourceDescriptor.java
+++ java/com/delcyon/capo/resourcemanager/types/JcrResourceDescriptor.java
@@ -17,13 +17,14 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 package com.delcyon.capo.resourcemanager.types;
 
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
 import java.util.EnumSet;
 
+import javax.jcr.Binary;
 import javax.jcr.Node;
 import javax.jcr.NodeIterator;
 import javax.jcr.Property;
@@ -32,10 +33,6 @@ import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 import javax.jcr.SimpleCredentials;
 import javax.jcr.Value;
-import javax.jcr.query.Query;
-import javax.jcr.query.QueryResult;
-
-import org.apache.jackrabbit.value.BinaryValue;
 
 import com.delcyon.capo.ContextThread;
 import com.delcyon.capo.datastream.stream_attribute_filter.ContentFormatTypeFilterInputStream;
@@ -45,8 +42,6 @@ import com.delcyon.capo.datastream.stream_attribute_filter.SizeFilterInputStream
 import com.delcyon.capo.resourcemanager.ResourceParameter;
 import com.delcyon.capo.server.jackrabbit.CapoJcrServer;
 import com.delcyon.capo.webapp.servlets.CapoWebApplication;
-import com.delcyon.capo.xml.XPath;
-import com.delcyon.capo.xml.cdom.CElement;
 import com.delcyon.capo.xml.cdom.VariableContainer;
 import com.delcyon.capo.xml.dom.ResourceDeclarationElement;
 
@@ -66,58 +61,56 @@ public class JcrResourceDescriptor extends AbstractResourceDescriptor
 	private String absPath = null;
 	private JcrContentMetaData jcrResourceMetatData = null;
 	private boolean isLocalSession = false;
-	private boolean isWriting = false;
+	private volatile Boolean isWriting = false;
+	private Boolean hasPipeThreadStarted = false;
 
+	 
+	
 	@Override
 	public void init(ResourceDeclarationElement declaringResourceElement,VariableContainer variableContainer, LifeCycle lifeCycle,boolean iterate, ResourceParameter... resourceParameters) throws Exception
 	{
-	    this.isLocalSession = false;
-		super.init(declaringResourceElement, variableContainer, lifeCycle, iterate,resourceParameters);
-		System.out.println("login");
-		
-		if(Thread.currentThread() instanceof ContextThread)
-		{
-		    this.session = ((ContextThread)Thread.currentThread()).getSession();
-		}
-		else if(WApplication.getInstance() != null)
-		{
-		    this.session = ((CapoWebApplication)WApplication.getInstance()).getJcrSession();
-		}
-		else if(CapoJcrServer.getRepository() != null && lifeCycle == LifeCycle.EXPLICIT)
-        {
-            this.session = CapoJcrServer.getRepository().login(new SimpleCredentials("admin","admin".toCharArray()));
-            this.isLocalSession = true;
-        }
-		if(this.session == null)
-		{
-		    throw new Exception("Can't use JCR resources without a Session");
-		}
-		
 		this.absPath = getResourceURI().getPath();
-		//dump(session.getRootNode());		
-		if(session.nodeExists(absPath) == false)
-		{
-		    String[] relPath = absPath.split("/");
-		    Node currentNode = session.getRootNode();
-		    for (String nodeName : relPath)
-            {
-		        if(nodeName.isEmpty())
-		        {
-		            currentNode = session.getRootNode();
-		            continue;
-		        }
-                if(currentNode.hasNode(nodeName) == false)
-                {
-                    currentNode.addNode(nodeName);//,"nt:folder");
-                }
-                currentNode = currentNode.getNode(nodeName);
-            }
-//		    session.getRootNode().addNode(getResourceURI().getResourceURIString().replaceFirst("^repo:/", ""));
-		}
-		
-		this.node = session.getNode(absPath);
+		super.init(declaringResourceElement, variableContainer, lifeCycle, iterate, resourceParameters);
 	}
 	
+	
+	@Override
+	public void open(VariableContainer variableContainer,ResourceParameter... resourceParameters) throws Exception
+	{
+		advanceState(State.INITIALIZED, variableContainer, resourceParameters);
+	    if (getResourceState().ordinal() < State.OPEN.ordinal())
+	    {
+	    	
+	    	this.isLocalSession = false;
+			
+			System.out.println("login");
+			
+			if(Thread.currentThread() instanceof ContextThread)
+			{
+			    this.session = ((ContextThread)Thread.currentThread()).getSession();
+			}
+			else if(WApplication.getInstance() != null)
+			{
+			    this.session = ((CapoWebApplication)WApplication.getInstance()).getJcrSession();
+			}
+			else if(CapoJcrServer.getRepository() != null && getLifeCycle() == LifeCycle.EXPLICIT)
+	        {
+	            this.session = CapoJcrServer.getRepository().login(new SimpleCredentials("admin","admin".toCharArray()));
+	            this.isLocalSession = true;
+	        }
+			if(this.session == null)
+			{
+			    throw new Exception("Can't use JCR resources without a Session");
+			}
+			
+			if(session.nodeExists(absPath) == true)
+			{
+				this.node = session.getNode(absPath);
+			}
+	        super.open(variableContainer,resourceParameters);
+	        ((JcrContentMetaData) getResourceMetaData(variableContainer, resourceParameters)).setNode(this.node);
+	    }
+	}
 	
 	@Override
 	public StreamType[] getSupportedStreamTypes() throws Exception
@@ -152,11 +145,12 @@ public class JcrResourceDescriptor extends AbstractResourceDescriptor
             setResourceState(State.STEPPING);
             return true;
         }
-        else
+        else if (getResourceState() == State.STEPPING)
         {
             setResourceState(State.OPEN);
             return false;
         }
+        return false;
 	}
 
 	@Override
@@ -191,60 +185,135 @@ public class JcrResourceDescriptor extends AbstractResourceDescriptor
 	@Override
 	public OutputStream getOutputStream(VariableContainer variableContainer,ResourceParameter... resourceParameters) throws Exception
 	{
-	    
-	   
-	    final PipedInputStream pipedInputStream = new PipedInputStream();
-	    
-	    PipedOutputStream pipedOutputStream = new PipedOutputStream(pipedInputStream);
-	   
-	    
-		//MimeTable mt = MimeTable.getDefaultTable();
-	    //String mimeType = mt.getContentTypeFor(file.getName());
-	    //if (mimeType == null) mimeType = "application/octet-stream";
+		
+	    synchronized (isWriting)
+		{
+	    	isWriting = true;
+	    	final PipedInputStream pipedInputStream = new PipedInputStream(){
+	    		@Override
+	    		public void close() throws IOException
+	    		{
+	    			System.out.println("piped input close attempt: "+System.currentTimeMillis());
+	    			// TODO Auto-generated method stub
+	    			super.close();
+	    			System.out.println("piped input closed: "+System.currentTimeMillis());
 
-	   // Node fileNode = node.addNode("<name>", "nt:file");
+	    		}
+	    	};
 
-	    //System.out.println( fileNode.getName() );
+	    	PipedOutputStream pipedOutputStream = new PipedOutputStream(pipedInputStream){
+	    		@Override
+	    		public void close() throws IOException
+	    		{	
+	    			System.out.println("piped output close attempt: "+System.currentTimeMillis());
+	    			
+	    			
+	    			super.close();
+	    			System.out.println("isWaiting = "+isWriting);
+	    			Boolean _isWaiting = null;
+	    			synchronized (isWriting)
+	    			{
+	    				_isWaiting = isWriting;
+	    			}
+	    				//don't let this close while we're still dealing with threaded output
+	    				System.out.println("isWaiting = "+_isWaiting);
+	    				while(_isWaiting == true )
+	    				{
+	    					try
+	    					{
+	    						System.out.println("waiting for pipe thread to finish");
+	    						Thread.sleep(100);
+	    						synchronized (isWriting)
+								{
+	    							_isWaiting = isWriting;
+								}
+	    					} catch (InterruptedException e)
+	    					{					
+	    						e.printStackTrace();
+	    					}
+	    				}
+	    			
+	    			System.out.println("piped output closed: "+System.currentTimeMillis());
+	    			
+	    			
+	    		}
+	    	};
 
-	    //final Node resNode = fileNode.addNode("jcr:content", "nt:resource");
-	    //resNode.setProperty("jcr:mimeType", "<mimeType>");
-	    //resNode.setProperty("jcr:encoding", "");
-	    
-	    Runnable pipe = new Runnable()
-        {
-            
-            @Override
-            public void run()
-            {
-                try
-                {
-                    isWriting = true;
-                    MD5FilterInputStream md5FilterInputStream = new MD5FilterInputStream(pipedInputStream);
-                    ContentFormatTypeFilterInputStream contentFormatTypeFilterInputStream = new ContentFormatTypeFilterInputStream(md5FilterInputStream);
-                    MimeTypeFilterInputStream mimeTypeFilterInputStream = new MimeTypeFilterInputStream(contentFormatTypeFilterInputStream);
-                    SizeFilterInputStream sizeFilterInputStream = new SizeFilterInputStream(mimeTypeFilterInputStream);
-                    node.setProperty("jcr:data",new BinaryValue(sizeFilterInputStream));
-                    node.setProperty(contentFormatTypeFilterInputStream.getName(),contentFormatTypeFilterInputStream.getValue());
-                    node.setProperty(sizeFilterInputStream.getName(),sizeFilterInputStream.getValue());                    
-                    node.setProperty(mimeTypeFilterInputStream.getName(),mimeTypeFilterInputStream.getValue());
-                    node.setProperty(md5FilterInputStream.getName(),md5FilterInputStream.getValue());                    
-                    System.out.println("pipe thread done");
-                }
-                catch (Exception e)
-                {
-                    e.printStackTrace();
-                }
-                isWriting = false;
-                
-            }
-        };
-	   
-        new Thread(pipe, node.getName()+" pipeThread").start();
-	    //Calendar lastModified = Calendar.getInstance();
-	    //lastModified.setTimeInMillis(file.lastModified());
-	    //resNode.setProperty("jcr:lastModified", lastModified);
-	    //Uti
-	    return pipedOutputStream;
+
+	    	//MimeTable mt = MimeTable.getDefaultTable();
+	    	//String mimeType = mt.getContentTypeFor(file.getName());
+	    	//if (mimeType == null) mimeType = "application/octet-stream";
+
+	    	// Node fileNode = node.addNode("<name>", "nt:file");
+
+	    	//System.out.println( fileNode.getName() );
+
+	    	//final Node resNode = fileNode.addNode("jcr:content", "nt:resource");
+	    	//resNode.setProperty("jcr:mimeType", "<mimeType>");
+	    	//resNode.setProperty("jcr:encoding", "");
+
+	    	Runnable pipe = new Runnable()
+	    	{
+
+	    		@Override
+	    		public void run()
+	    		{
+	    			
+
+	    				try
+	    				{
+	    					
+
+	    					MD5FilterInputStream md5FilterInputStream = new MD5FilterInputStream(pipedInputStream);
+	    					ContentFormatTypeFilterInputStream contentFormatTypeFilterInputStream = new ContentFormatTypeFilterInputStream(md5FilterInputStream);
+	    					MimeTypeFilterInputStream mimeTypeFilterInputStream = new MimeTypeFilterInputStream(contentFormatTypeFilterInputStream);
+	    					SizeFilterInputStream sizeFilterInputStream = new SizeFilterInputStream(mimeTypeFilterInputStream);
+	    					System.out.println("pipe thread start read: "+System.currentTimeMillis());
+	    					synchronized (hasPipeThreadStarted)
+							{
+	    						hasPipeThreadStarted.notify();
+							}	    					
+	    					Binary binary = node.getSession().getValueFactory().createBinary( sizeFilterInputStream );
+	    					node.setProperty("jcr:data",binary);
+	    					binary.dispose();                    
+	    					node.setProperty(contentFormatTypeFilterInputStream.getName(),contentFormatTypeFilterInputStream.getValue());
+	    					node.setProperty(sizeFilterInputStream.getName(),sizeFilterInputStream.getValue());                    
+	    					node.setProperty(mimeTypeFilterInputStream.getName(),mimeTypeFilterInputStream.getValue());
+	    					node.setProperty(md5FilterInputStream.getName(),md5FilterInputStream.getValue());                    
+	    					System.out.println("pipe thread done: "+System.currentTimeMillis());
+	    				}
+	    				catch (Exception e)
+	    				{
+	    					e.printStackTrace();            		
+	    				}
+	    				finally
+	    				{
+	    					//isWriting.notify();
+	    					synchronized (isWriting)
+	    	    			{
+	    						isWriting = false;
+	    	    			}
+	    				}
+	    			}
+	    		
+	    	};
+
+	    	synchronized (hasPipeThreadStarted)
+			{
+	    		new Thread(pipe, node.getName()+" pipeThread-"+System.currentTimeMillis()).start();
+	    		//Calendar lastModified = Calendar.getInstance();
+	    		//lastModified.setTimeInMillis(file.lastModified());
+	    		//resNode.setProperty("jcr:lastModified", lastModified);
+	    		//Uti
+
+
+				System.out.println("waiting for pipe thread to start");
+				hasPipeThreadStarted.wait(1500);
+			}
+			
+			
+	    	return pipedOutputStream;
+		}
 	}
 	
 	@Override
@@ -260,58 +329,88 @@ public class JcrResourceDescriptor extends AbstractResourceDescriptor
 	    }
 	}
 	
-	@Override
-	public void writeXML(VariableContainer variableContainer, CElement element, ResourceParameter... resourceParameters) throws Exception
-	{
-	    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-	    XPath.dumpNode(element, outputStream);
-	    session.importXML(getResourceURI().getResourceURIString().replaceFirst("^repo:", ""), new ByteArrayInputStream(outputStream.toByteArray()), 0);
-	    dump(session.getRootNode());
-	    session.save();
-	    String[] langs = session.getWorkspace().getQueryManager().getSupportedQueryLanguages();
-	    String[] prefixes = session.getNamespacePrefixes();
-	    
-	    Query query = session.getWorkspace().getQueryManager().createQuery("SELECT * FROM [nt:unstructured] where NAME([nt:unstructured]) = 'server:log' order by message", "JCR-SQL2");
-	    QueryResult result = query.execute();
-
-
-	    // Iterate over the nodes in the results ...
-
-	    NodeIterator nodeIter = result.getNodes();
-	    System.out.println("=============================");
-	    while ( nodeIter.hasNext() ) {
-
-	        Node _node = nodeIter.nextNode();
-	        System.out.println("===>"+_node.getName()+" type:"+_node.getPrimaryNodeType().getName());
-	        dump(_node);
-
-	    }
-	    System.out.println("=============================");
-	    session.logout();
-	}
+//	@Override
+//	public void writeXML(VariableContainer variableContainer, CElement element, ResourceParameter... resourceParameters) throws Exception
+//	{
+//	    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+//	    XPath.dumpNode(element, outputStream);
+//	    session.importXML(getResourceURI().getResourceURIString().replaceFirst("^repo:", ""), new ByteArrayInputStream(outputStream.toByteArray()), 0);
+//	    dump(session.getRootNode());
+//	    session.save();
+//	    String[] langs = session.getWorkspace().getQueryManager().getSupportedQueryLanguages();
+//	    String[] prefixes = session.getNamespacePrefixes();
+//	    
+//	    Query query = session.getWorkspace().getQueryManager().createQuery("SELECT * FROM [nt:unstructured] where NAME([nt:unstructured]) = 'server:log' order by message", "JCR-SQL2");
+//	    QueryResult result = query.execute();
+//
+//
+//	    // Iterate over the nodes in the results ...
+//
+//	    NodeIterator nodeIter = result.getNodes();
+//	    System.out.println("=============================");
+//	    while ( nodeIter.hasNext() ) {
+//
+//	        Node _node = nodeIter.nextNode();
+//	        System.out.println("===>"+_node.getName()+" type:"+_node.getPrimaryNodeType().getName());
+//	        dump(_node);
+//
+//	    }
+//	    System.out.println("=============================");
+//	    session.logout();
+//	}
 	
 	
 	@Override
 	public boolean performAction(VariableContainer variableContainer,Action action,ResourceParameter... resourceParameters) throws Exception
 	{
 	    super.addResourceParameters(variableContainer, resourceParameters);
-	    
+	    advanceState(State.OPEN, variableContainer, resourceParameters);
         
         boolean success = false;
 
         if (action == Action.CREATE)
         {
-           System.out.println(node.isNew());
-           //node.setPrimaryType("nt:folder");
-           
-           
+        	if(session.nodeExists(absPath) == false)
+    		{
+    		    String[] relPath = absPath.split("/");
+    		    Node currentNode = session.getRootNode();
+    		    for (String nodeName : relPath)
+                {
+    		        if(nodeName.isEmpty())
+    		        {
+    		            currentNode = session.getRootNode();
+    		            continue;
+    		        }
+                    if(currentNode.hasNode(nodeName) == false)
+                    {
+                        currentNode.addNode(nodeName);//,"nt:folder");
+                    }
+                    currentNode = currentNode.getNode(nodeName);
+                }
+    		    //session.getRootNode().addNode(getResourceURI().getResourceURIString().replaceFirst("^repo:/", ""));
+    		    this.node = session.getNode(absPath);
+    		    ((JcrContentMetaData) getResourceMetaData(variableContainer, resourceParameters)).setNode(this.node);
+    		    refreshResourceMetaData(variableContainer, resourceParameters);
+    		    success = true;
+    		}
         }
         else if (action == Action.DELETE)
         {
             if (node != null)
             {
                 node.remove();
-           
+                this.node = null;
+    		    ((JcrContentMetaData) getResourceMetaData(variableContainer, resourceParameters)).setNode(this.node);
+    		    refreshResourceMetaData(variableContainer, resourceParameters);
+    		    success = true;
+            }
+        }
+        else if (action == Action.COMMIT)
+        {
+            if (node != null)
+            {
+                node.getSession().save();
+    		    success = true;
             }
         }
 	    return success;
@@ -363,16 +462,14 @@ public class JcrResourceDescriptor extends AbstractResourceDescriptor
 	@Override
 	public void close(VariableContainer variableContainer, ResourceParameter... resourceParameters) throws Exception
 	{	 
-	    //don't let this close while we're still dealing with threaded output
-	    while(isWriting == true)
-	    {
-	        Thread.sleep(10);
-	    }
-	    super.close(variableContainer, resourceParameters);
-	    if(isLocalSession == true && session.isLive())
+		super.close(variableContainer, resourceParameters);
+	    if(isLocalSession == true && session != null && session.isLive())
 	    {
 	        session.logout();
+	        ((JcrContentMetaData) getResourceMetaData(variableContainer, resourceParameters)).setNode(null);
+		    this.session = null;
 	    }
+	    
 	    
 	}
 	
