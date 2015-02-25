13ae257acb01718b687b3f60b02b9687219925a4
Jeremiah Jahn <jeremiah@delcyon.com>
2015-02-25 15:11:14 -0600

    seperated out Resource explorer panes from main web application. Navigation is next.

--- java/com/delcyon/capo/webapp/servlets/CapoWebApplication.java
+++ java/com/delcyon/capo/webapp/servlets/CapoWebApplication.java
@@ -1,30 +1,21 @@
 
 package com.delcyon.capo.webapp.servlets;
 
-import java.util.List;
-import java.util.SortedSet;
-
 import javax.jcr.Session;
 
 import com.delcyon.capo.CapoApplication;
 import com.delcyon.capo.resourcemanager.ResourceDescriptor;
 import com.delcyon.capo.resourcemanager.ResourceDescriptor.Action;
 import com.delcyon.capo.resourcemanager.ResourceDescriptor.State;
-import com.delcyon.capo.resourcemanager.ResourceURI;
 import com.delcyon.capo.resourcemanager.types.FileResourceType;
 import com.delcyon.capo.server.jackrabbit.CapoJcrServer;
-import com.delcyon.capo.webapp.models.DomItemModel.DomUse;
-import com.delcyon.capo.webapp.models.ResourceDescriptorItemModel;
-import com.delcyon.capo.webapp.widgets.WCapoResourceEditor;
-import com.delcyon.capo.webapp.widgets.WCapoResourceTreeView;
+import com.delcyon.capo.webapp.widgets.WCapoResourceExplorer;
 import com.delcyon.capo.webapp.widgets.WCapoSearchControl;
 import com.delcyon.capo.xml.dom.ResourceDocument;
 
 import eu.webtoolkit.jwt.AlignmentFlag;
-import eu.webtoolkit.jwt.MatchOptions;
 import eu.webtoolkit.jwt.PositionScheme;
 import eu.webtoolkit.jwt.Signal;
-import eu.webtoolkit.jwt.Signal2;
 import eu.webtoolkit.jwt.WApplication;
 import eu.webtoolkit.jwt.WBootstrapTheme;
 import eu.webtoolkit.jwt.WBoxLayout;
@@ -34,16 +25,12 @@ import eu.webtoolkit.jwt.WEnvironment;
 import eu.webtoolkit.jwt.WGridLayout;
 import eu.webtoolkit.jwt.WLength;
 import eu.webtoolkit.jwt.WLink;
-import eu.webtoolkit.jwt.WLink.Type;
 import eu.webtoolkit.jwt.WMenuItem;
-import eu.webtoolkit.jwt.WModelIndex;
-import eu.webtoolkit.jwt.WMouseEvent;
 import eu.webtoolkit.jwt.WNavigationBar;
 import eu.webtoolkit.jwt.WPopupMenu;
 import eu.webtoolkit.jwt.WPushButton;
 import eu.webtoolkit.jwt.WStackedWidget;
 import eu.webtoolkit.jwt.WTabWidget;
-import eu.webtoolkit.jwt.WVBoxLayout;
 import eu.webtoolkit.jwt.WWidget;
 
 public class CapoWebApplication extends WApplication {
@@ -52,16 +39,10 @@ public class CapoWebApplication extends WApplication {
 	    
     private WContainerWidget rootContainerWidget;
 	private WGridLayout rootLayout;
-	private WContainerWidget contentPane;
-	private WGridLayout contentPaneLayout;
-	private WVBoxLayout detailsPaneLayout;
-	private WCapoResourceEditor capoResourceEditor;
+	private WCapoResourceExplorer capoResourceExplorer;
 	private WTabWidget subDetailsPane;
-	private WCapoResourceTreeView treeView;
-    private ResourceDocument document;
+	private ResourceDocument document;
     private Session jcrSession;
-    private WPushButton resetButton;
-    private WPushButton saveButton;
     private WCapoSearchControl capoSearchControl;
    
     
@@ -84,7 +65,7 @@ public class CapoWebApplication extends WApplication {
         }
         
         createUI();
-        WApplication.getInstance().internalPathChanged().addListener(this,this::processInternalPathChange);
+        //WApplication.getInstance().internalPathChanged().addListener(this,this::processInternalPathChange);
     }
 
 	
@@ -98,7 +79,7 @@ public class CapoWebApplication extends WApplication {
        
         
         getRootLayout().addWidget(getNavigationContainer(),0,0);              
-        getRootLayout().addWidget(getContentPane(),1,0);
+        getRootLayout().addWidget(getCapoResourceExplorer(),1,0);
         try
 		{
             FileResourceType fileResourceType = new FileResourceType();
@@ -114,7 +95,7 @@ public class CapoWebApplication extends WApplication {
 //            document = (ResourceDocument) documentBuilder.buildDocument(resourceDescriptor2);
             
 			
-			getTreeView().setRootResourceDescriptor(resourceDescriptor);
+            getCapoResourceExplorer().setRootResourceDescriptor(resourceDescriptor);
 			
 		} catch (Exception e)
 		{			
@@ -122,118 +103,28 @@ public class CapoWebApplication extends WApplication {
 		}
         
         //pathButton.setLink(new WLink(Type.InternalPath, "/legend"));
-        getContentPaneLayout().addWidget(getTreeView(), 0, 0,1,0);
-        getContentPaneLayout().addLayout(getDetailsPaneLayout(), 0, 1);
-        getContentPaneLayout().addWidget(getSaveButton(), 2, 0, 1, 1, AlignmentFlag.AlignTop); 
-        getContentPaneLayout().addWidget(getResetButton(), 3, 0, 1, 1, AlignmentFlag.AlignTop);
-        getDetailsPaneLayout().addWidget(getDetailsPane(), 0);
-        getDetailsPaneLayout().setResizable(0);
+        
             
     }
 
-    private WPushButton getSaveButton()
+    
+	
+    public Session getJcrSession()
     {
-        if(saveButton == null)
-        {
-            saveButton = new WPushButton("Save");
-            saveButton.clicked().addListener(this, this::saveSession);
-        }
-        return saveButton;
+        return this.jcrSession;
     }
     
-    /**
-     * saves current jcrSession, TODO this should really work with the current users workspace or something.
-     */
-    private void saveSession()
-    {
-        try
-        {
-            jcrSession.save();
-        } catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-    }
-    
-    private WPushButton getResetButton()
-    {
-        
-        if(resetButton == null)
-        {
-            resetButton = new WPushButton("Reset");
-            resetButton.setLink(new WLink(Type.InternalPath, "/"));
-            resetButton.clicked().addListener(this, this::reset);           
-        }
-        return resetButton;
-    }
-    
-    /**
-     * Resets session and tree view to root
-     */
-    private void reset()
-    {
-        try
-        {
-            jcrSession.refresh(false);
-            ((ResourceDescriptorItemModel) treeView.getModel()).reload();
-            treeView.setModel(new ResourceDescriptorItemModel(CapoApplication.getDataManager().getResourceDescriptor(null, "repo:/"),DomUse.NAVIGATION));
-        } catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-    }
-    
-    private WTabWidget getSubDetailsPane() {
-    	if (subDetailsPane == null)
+	
+
+	private WCapoResourceExplorer getCapoResourceExplorer() {
+    	if (capoResourceExplorer == null)
     	{
-    		subDetailsPane = new WTabWidget();
+    		capoResourceExplorer = new WCapoResourceExplorer();
     	}
-		return subDetailsPane;
+    	return capoResourceExplorer;
 	}
 
-	private WCapoResourceEditor getDetailsPane() 
-    {
-    	if (capoResourceEditor == null)
-    	{
-    		capoResourceEditor = new WCapoResourceEditor();
-    	
-    	}
-    	return capoResourceEditor;
-	}
-
-	private WBoxLayout getDetailsPaneLayout() {
-		if (detailsPaneLayout == null)
-		{
-			detailsPaneLayout = new WVBoxLayout();
-		}
-    	return detailsPaneLayout;
-	}
-
-	private WWidget getContentPane() {
-    	if (contentPane == null)
-    	{
-    		contentPane = new WContainerWidget();
-    		contentPane.setMargin(0);
-    		contentPane.setLayout(getContentPaneLayout());
-    		//contentPane.setAttributeValue("style", "background-image: url('/wr/images/background.png'); background-repeat: no-repeat; background-position: bottom right; background-size: contain;");
-    	
-    	
-    	}
-    	return contentPane;
-	}
-
-	private WGridLayout getContentPaneLayout() {
-		if (contentPaneLayout == null)
-		{
-			contentPaneLayout = new WGridLayout();			
-			contentPaneLayout.setColumnStretch(1, 1);
-			contentPaneLayout.setContentsMargins(0, 0, 0, 0);
-			contentPaneLayout.setColumnResizable(0);
-			contentPaneLayout.setRowStretch(1, 1);
-			contentPaneLayout.setRowStretch(0, 1);
-		}
-		 return contentPaneLayout;
-	}
+	
 
 	private WGridLayout getRootLayout() {
     	if (rootLayout == null)
@@ -245,97 +136,14 @@ public class CapoWebApplication extends WApplication {
     	return rootLayout;
 	}
     
-    private  WCapoResourceTreeView getTreeView() {
-        if(treeView == null)
-        {
-            treeView = new WCapoResourceTreeView();
-            //watch for selection change events
-            treeView.selectionChanged().addListener(this, this::selectedItemChanged);
-            //watch for internal patch change requests from tree
-            treeView.doubleClicked().addListener(this, this::processTreeDoubleClick);
-        }       
-
-        return treeView;
-    }
-    
-    /**
-     * Process request for tree root/internal path changes from tree
-     * @param arg1
-     * @param arg2
-     */
-    private void processTreeDoubleClick(WModelIndex arg1, WMouseEvent arg2)
-    {
-        setInternalPath(((ResourceDescriptor)arg1.getInternalPointer()).getResourceURI().getPath(), false);
-        getTreeView().setModel(new ResourceDescriptorItemModel((ResourceDescriptor)arg1.getInternalPointer(),DomUse.NAVIGATION));
-    }
-    
-    /**
-     * sends selection changes in the tree to the resource editor
-     */
-    private void selectedItemChanged()
-    {
-        SortedSet<WModelIndex> selectedIndexes = getTreeView().getSelectedIndexes();
-        if(selectedIndexes.size() == 0)
-        {
-            return;
-        }
-        else if (selectedIndexes.size() == 1)
-        {
-            WModelIndex modelIndex = selectedIndexes.first();
-            final Object selectedItem =  modelIndex.getInternalPointer();           
-            getDetailsPane().setModel(selectedItem); //TODO add somesort of event/Signal listener to process this            
-        }
-        refresh();
-    }
     
     
     
-    /**
-     * Deal with any internal path changes in the system. Make sure that the proper components and resources are loaded and views are show etc
-     */
-    private void processInternalPathChange()
-    {
-        System.out.println(WApplication.getInstance().getInternalPath());
-        try
-        {
-            
-            ResourceDescriptor resourceDescriptor = CapoApplication.getDataManager().getResourceDescriptor(null, "repo:"+WApplication.getInstance().getInternalPath());
-            ResourceURI originalURI = resourceDescriptor.getResourceURI();
-            if(resourceDescriptor.getResourceMetaData(null).isContainer() == false)
-            {
-                if(resourceDescriptor.getParentResourceDescriptor() != null)
-                {
-                    resourceDescriptor = resourceDescriptor.getParentResourceDescriptor();
-                }
-                else
-                {
-                    String parentURI = resourceDescriptor.getResourceURI().getResourceURIString().replaceAll("/"+resourceDescriptor.getLocalName(), "");
-                    if(parentURI.equals("repo:")) //make sure we have a root for the repo
-                    {
-                        parentURI = "repo:/";
-                    }
-                    
-                    resourceDescriptor = CapoApplication.getDataManager().getResourceDescriptor(null,parentURI);
-                    setInternalPath(resourceDescriptor.getResourceURI().getPath());
-                }
-            }
-            getTreeView().setRootResourceDescriptor(resourceDescriptor);
-            if(originalURI != null)
-            {
-                List<WModelIndex> indexes = getTreeView().getModel().match(getTreeView().getModel().getIndex(0, 0), ResourceDescriptorItemModel.ResourceURI_ROLE, originalURI.toString(), 1, MatchOptions.defaultMatchOptions);
-                if(indexes.size() > 0)
-                {
-                    getTreeView().select(indexes.get(0));
-                }
-            }
-            getTreeView().selectionChanged();
-        }
-        catch (Exception e)
-        {                         
-            e.printStackTrace();
-        }
     
-    }
+    
+    
+    
+    
    
     
     
@@ -344,9 +152,6 @@ public class CapoWebApplication extends WApplication {
     private WWidget getNavigationContainer()
     {
     	//create and make nav container and nav bar widget
-        //WContainerWidget navContainer = new WContainerWidget();
-       
-       
         WNavigationBar navigation = new WNavigationBar();
        
         
@@ -531,10 +336,6 @@ public class CapoWebApplication extends WApplication {
         return container;
     }
 
-    public Session getJcrSession()
-    {
-        return this.jcrSession;
-    }
-    
+   
     
 }
