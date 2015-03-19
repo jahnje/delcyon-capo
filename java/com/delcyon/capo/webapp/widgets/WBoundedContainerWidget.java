68ee5c664844d72a9defa92cb8e9dff709fec5e7
Jeremiah Jahn <jeremiah@delcyon.com>
2015-03-19 13:09:52 -0500

    Added auto scolling, and multiple internalized containers.

--- java/com/delcyon/capo/webapp/widgets/WBoundedContainerWidget.java
+++ java/com/delcyon/capo/webapp/widgets/WBoundedContainerWidget.java
@@ -9,6 +9,7 @@ import eu.webtoolkit.jwt.WBoxLayout.Direction;
 import eu.webtoolkit.jwt.WContainerWidget;
 import eu.webtoolkit.jwt.WMouseEvent;
 import eu.webtoolkit.jwt.WPushButton;
+import eu.webtoolkit.jwt.WScrollArea;
 import eu.webtoolkit.jwt.WText;
 import eu.webtoolkit.jwt.WToolBar;
 import eu.webtoolkit.jwt.WWidget;
@@ -18,25 +19,45 @@ public class WBoundedContainerWidget extends WContainerWidget
 
     
     private WText title = new WText();
-    private WBoxLayout layout = new WBoxLayout(Direction.TopToBottom);    
+    private WBoxLayout internalLayout = new WBoxLayout(Direction.TopToBottom);
+    private WBoxLayout layout = new WBoxLayout(Direction.TopToBottom);
     private WToolBar toolBar = new WToolBar();
+    private WContainerWidget internalContainer = new WContainerWidget();
     
     public WBoundedContainerWidget()
     {
+        this(null);
         
+    }
+    
+    public WBoundedContainerWidget(WContainerWidget parent)
+    {
+        super(parent);
         setLayout(layout);
-        setInline(false);        
-        layout.setSpacing(1);
-        layout.addWidget(toolBar, 0);
-        layout.addWidget(title,0);
+        setInline(false);
         
-        addStyleClass("bounded_container");
-        //setLayoutSizeAware(true);
+        internalContainer.setLayout(internalLayout);
+        internalContainer.setInline(false);
+                
+        layout.setSpacing(-1);
+        layout.addWidget(toolBar,0);
+        layout.addWidget(title,0);
+        layout.addWidget(internalContainer,1);
+        
+        super.addStyleClass("bounded_container",false);
+        setLayoutSizeAware(true);
         title.setTextAlignment(AlignmentFlag.AlignCenter);
         title.setInline(false);
         title.addStyleClass("h2");
         
     }
+    
+    @Override
+    public void addStyleClass(String styleClass, boolean force)
+    {        
+        internalContainer.addStyleClass(styleClass, force);
+    }
+    
     
     /**
      * Adds a widget to the layout.
@@ -45,7 +66,7 @@ public class WBoundedContainerWidget extends WContainerWidget
      * addWidget(widget, 0, EnumSet.noneOf(AlignmentFlag.class))}
      */
     public final void addLayoutWidget(WWidget widget) {
-        layout.addWidget(widget);
+        internalLayout.addWidget(widget,0);
     }
 
     /**
@@ -55,7 +76,21 @@ public class WBoundedContainerWidget extends WContainerWidget
      * addWidget(widget, stretch, EnumSet.noneOf(AlignmentFlag.class))}
      */
     public final void addLayoutWidget(WWidget widget, int stretch) {
-        layout.addWidget(widget, stretch);
+        if(stretch > 0)
+        {
+            if(widget instanceof WContainerWidget)
+            {
+                ((WContainerWidget) widget).setOverflow(Overflow.OverflowAuto);
+            }
+            else
+            {
+                WScrollArea scrollArea = new WScrollArea();
+                scrollArea.setWidget(widget);
+                widget = scrollArea;
+            }
+            
+        }
+        internalLayout.addWidget(widget, stretch);
     }
     
     public void setTitle(String title)
